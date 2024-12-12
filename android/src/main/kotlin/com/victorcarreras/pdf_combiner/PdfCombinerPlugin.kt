a50c08755dd8275d98def4a4a6386c6fb7305f28
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2024-12-12 09:48:34 +0100

    Android old code copied

--- android/src/main/kotlin/com/victorcarreras/pdf_combiner/PdfCombinerPlugin.kt
+++ android/src/main/kotlin/com/victorcarreras/pdf_combiner/PdfCombinerPlugin.kt
@@ -1,6 +1,10 @@
 package com.victorcarreras.pdf_combiner
 
 import androidx.annotation.NonNull
+import android.content.Context
+import com.ril.pdf_merger.CreateImageFromPDF
+import com.ril.pdf_merger.CreatePDFFromMultipleImage
+import com.ril.pdf_merger.MergeMultiplePDF
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -15,15 +19,23 @@ class PdfCombinerPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var context: Context
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "pdf_combiner")
     channel.setMethodCallHandler(this)
+    this.context = flutterPluginBinding.applicationContext
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "mergeMultiplePDF") {
+      MergeMultiplePDF(context, result).merge(call.argument("paths"), call.argument("outputDirPath"))
+    } else if (call.method == "createPDFFromMultipleImage") {
+      CreatePDFFromMultipleImage(context, result).create(call.argument("paths"), call.argument("outputDirPath"), call.argument("needImageCompressor")
+        , call.argument("maxWidth"), call.argument("maxHeight"))
+    } else if (call.method == "createImageFromPDF") {
+      CreateImageFromPDF(context, result).create(call.argument("path"), call.argument("outputDirPath")
+        , call.argument("maxWidth"), call.argument("maxHeight"), call.argument("createOneImage"))
     } else {
       result.notImplemented()
     }
