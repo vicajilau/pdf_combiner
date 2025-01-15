a3706c45e7a2a19606d321bed3ef0b2766db7078
DanielJimen <daniel.jimenezrodriguez@axa.ch>
2025-01-15 13:00:37 +0100

    [origin/feature/implement_web] - remove warning

--- test/pdf_combiner_method_channel_test.dart
+++ test/pdf_combiner_method_channel_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pdf_combiner/communication/pdf_combiner_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelPdfCombiner platform = MethodChannelPdfCombiner();
   const MethodChannel channel = MethodChannel('pdf_combiner');
 
   setUp(() {
