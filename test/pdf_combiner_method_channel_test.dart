8b2cbece46198b0a6e40308f6dec08b6850af9e7
DanielJimen <daniel.jimenezrodriguez@axa.ch>
2025-01-27 11:32:56 +0100

    [origin/feature/implement_web] - resolve tests

--- test/pdf_combiner_method_channel_test.dart
+++ test/pdf_combiner_method_channel_test.dart
@@ -2,32 +2,13 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pdf_combiner/communication/pdf_combiner_method_channel.dart';
 
+import 'mocks/mock_pdf_combiner_platform.dart';
+
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelPdfCombiner platform = MethodChannelPdfCombiner();
+  MockPdfCombinerPlatform platform = MockPdfCombinerPlatform();
   const MethodChannel channel = MethodChannel('pdf_combiner');
-
-  setUp(() {
-    // Mock method call handler for testing platform methods.
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        if (methodCall.method == 'mergeMultiplePDF') {
-          // Simulate a successful merge and return a dummy result.
-          return 'Merge successful';
-        } else if (methodCall.method == 'createImageFromPDF') {
-          // Simulate a successful merge and return a dummy result.
-          return ['Merge successful'];
-        } else if (methodCall.method == 'createPDFFromMultipleImage') {
-          // Simulate a successful merge and return a dummy result.
-          return 'Merge successful';
-        }
-        return null;
-      },
-    );
-  });
 
   tearDown(() {
     // Clean up the mock after each test.
@@ -48,7 +29,7 @@ void main() {
       );
 
       // Assert
-      expect(result, 'Merge successful');
+      expect(result, 'Merged PDF');
     });
 
     test('createImageFromPDF returns success message', () async {
@@ -57,13 +38,13 @@ void main() {
       String outputDirPath = '/path/to/output/';
 
       // Act
-      final result = await platform.createImageFromPDF(
+      final List<String>? result = await platform.createImageFromPDF(
         inputPath: path,
         outputPath: outputDirPath,
       );
 
       // Assert
-      expect(result, ['Merge successful']);
+      expect(result, ['image1.png', 'image2.png']);
     });
 
     test('createPDFFromMultipleImages returns success message', () async {
@@ -78,7 +59,7 @@ void main() {
       );
 
       // Assert
-      expect(result, 'Merge successful');
+      expect(result, 'Created PDF from Images');
     });
   });
 }
