8fc92c3412dfa6e94da7f97ca9f538c63a043674
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-02-18 07:59:42 +0100

    increased coverage

--- test/pdf_combiner_method_channel_test.dart
+++ test/pdf_combiner_method_channel_test.dart
@@ -1,71 +1,44 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pdf_combiner/communication/pdf_combiner_method_channel.dart';
+import 'package:pdf_combiner/communication/pdf_combiner_platform_interface.dart';
+
+class TestPdfCombinerPlatform extends PdfCombinerPlatform {}
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  const MethodChannel channel = MethodChannel('pdf_combiner');
-  final MethodChannelPdfCombiner platform = MethodChannelPdfCombiner();
+  late PdfCombinerPlatform platform;
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
-      switch (methodCall.method) {
-        case 'mergeMultiplePDF':
-          return 'Merged PDF';
-        case 'createImageFromPDF':
-          return methodCall.arguments['createOneImage'] == true
-              ? ['image1.png']
-              : ['image1.png', 'image2.png'];
-        case 'createPDFFromMultipleImage':
-          return 'Created PDF from Images';
-        default:
-          return null;
-      }
-    });
+    platform = TestPdfCombinerPlatform();
   });
 
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(channel, null);
-  });
-
-  group('MethodChannelPdfCombiner Unit Tests', () {
-    test('mergeMultiplePDF returns success message', () async {
-      final result = await platform.mergeMultiplePDFs(
-        inputPaths: ['file1.pdf', 'file2.pdf'],
-        outputPath: '/path/to/output/',
+  group('PdfCombinerPlatform UnimplementedError Tests', () {
+    test('mergeMultiplePDFs should throw UnimplementedError', () {
+      expect(
+            () async => await platform.mergeMultiplePDFs(
+          inputPaths: ['file1.pdf', 'file2.pdf'],
+          outputPath: '/path/to/output/',
+        ),
+        throwsUnimplementedError,
       );
-      expect(result, 'Merged PDF');
     });
 
-    test('createImageFromPDF returns success message with multiple images',
-        () async {
-      final result = await platform.createImageFromPDF(
-        inputPath: 'file1.pdf',
-        outputPath: '/path/to/output/',
-        createOneImage: false,
+    test('createPDFFromMultipleImages should throw UnimplementedError', () {
+      expect(
+            () async => await platform.createPDFFromMultipleImages(
+          inputPaths: ['image1.jpg', 'image2.png'],
+          outputPath: '/path/to/output/',
+        ),
+        throwsUnimplementedError,
       );
-      expect(result, ['image1.png', 'image2.png']);
     });
 
-    test('createImageFromPDF returns success message with a single image',
-        () async {
-      final result = await platform.createImageFromPDF(
-        inputPath: 'file1.pdf',
-        outputPath: '/path/to/output/',
-        createOneImage: true,
+    test('createImageFromPDF should throw UnimplementedError', () {
+      expect(
+            () async => await platform.createImageFromPDF(
+          inputPath: 'file1.pdf',
+          outputPath: '/path/to/output/',
+        ),
+        throwsUnimplementedError,
       );
-      expect(result, ['image1.png']);
-    });
-
-    test('createPDFFromMultipleImages returns success message', () async {
-      final result = await platform.createPDFFromMultipleImages(
-        inputPaths: ['file1.jpg', 'file2.png'],
-        outputPath: '/path/to/output/',
-      );
-      expect(result, 'Created PDF from Images');
     });
   });
 }
