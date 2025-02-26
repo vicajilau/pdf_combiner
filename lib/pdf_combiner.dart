2d2872bb854ce81fa181a652bf98705f0b80aa91
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-02-26 11:27:15 +0100

    Renamed ImageQuality to ImageCompression

--- lib/pdf_combiner.dart
+++ lib/pdf_combiner.dart
@@ -87,7 +87,6 @@ class PdfCombiner {
   /// - `outputPath`: The directory path where the created PDF should be saved.
   /// - `config`: A configuration object that specifies how to process the images.
   ///   - `rescale`: The scaling configuration for the images (default is the original image).
-  ///   - `compression`: The image quality level for compression, affecting file size and clarity (default is [ImageQuality.high]).
   ///   - `keepAspectRatio`: Indicates whether to maintain the aspect ratio of the images (default is `true`).
   ///
   /// Returns:
@@ -167,7 +166,7 @@ class PdfCombiner {
   /// - `outputDirPath`: A string representing the directory where the list of images should be saved.
   /// - `config`: A configuration object that specifies how to process the images.
   ///   - `rescale`: The scaling configuration for the images (default is the original image).
-  ///   - `compression`: The image quality level for compression, affecting file size and clarity (default is [ImageQuality.high]).
+  ///   - `compression`: The image compression level for the images, affecting file size, quality and clarity (default is [ImageCompression.none]).
   ///   - `createOneImage`: Indicates whether to create a single image or separate images for each page (default is `true`).
   ///
   /// Returns:
