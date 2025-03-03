b61ead426fafedd078a4d901f303930245ba265b
DanielJimen <daniel.jimenezrodriguez@axa.ch>
2025-03-03 09:20:11 +0100

    [feature/v4_web_refactor] - scale image into web

--- lib/pdf_combiner_web.dart
+++ lib/pdf_combiner_web.dart
@@ -96,7 +96,7 @@ class PdfCombinerWeb extends PdfCombinerPlatform {
   }) async {
     final JSArray<JSString> jsInputPaths = inputPaths.toJSArray();
     final JSString result =
-        (await createPdfFromImages(jsInputPaths).toDart) as JSString;
+        (await createPdfFromImages(jsInputPaths,config.jsify()).toDart) as JSString;
     return result.toDart;
   }
 
@@ -124,8 +124,8 @@ class PdfCombinerWeb extends PdfCombinerPlatform {
   }) async {
     final JSString jsInputPath = inputPath.toJS;
     final JSArray<JSString> result = config.createOneImage
-        ? (await pdfToImage(jsInputPath).toDart) as JSArray<JSString>
-        : (await convertPdfToImages(jsInputPath).toDart) as JSArray<JSString>;
+        ? (await pdfToImage(jsInputPath,config.jsify()).toDart) as JSArray<JSString>
+        : (await convertPdfToImages(jsInputPath,config.jsify()).toDart) as JSArray<JSString>;
     return result.toList();
   }
 }
