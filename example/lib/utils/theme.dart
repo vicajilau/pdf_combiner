a021de7e2c14ab500b4e84d71fe73f1795b056b2
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-05-22 07:13:36 +0200

    Updated flutter 3.32

--- example/lib/utils/theme.dart
+++ example/lib/utils/theme.dart
@@ -57,11 +57,6 @@ class AppTheme {
         ),
       ),
     ),
-    cardTheme: const CardTheme(
-      color: surfaceColor,
-      elevation: 2.0,
-      margin: EdgeInsets.all(8.0),
-    ),
   );
 
   static final ThemeData darkTheme = ThemeData(
@@ -105,11 +100,6 @@ class AppTheme {
           borderRadius: BorderRadius.all(Radius.circular(8.0)),
         ),
       ),
-    ),
-    cardTheme: const CardTheme(
-      color: Color(0xFF1E1E1E),
-      elevation: 2.0,
-      margin: EdgeInsets.all(8.0),
     ),
   );
 }
