9def0851ab66617f045351891bd61bdfa7da0788
kimseokho582 <gjrehf@naver.com>
2025-07-07 18:58:49 +0900

    change isPDF

--- lib/utils/document_utils.dart
+++ lib/utils/document_utils.dart
@@ -11,10 +11,36 @@ class DocumentUtils {
   ///
   /// This method checks if the file path ends with the `.pdf` extension
   /// (case insensitive).
+  // static Future<bool> isPDF(String filePath) async {
+  //   try {
+  //     return await FileMagicNumber.detectFileTypeFromPathOrBlob(filePath) ==
+  //         FileMagicNumberType.pdf;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
   static Future<bool> isPDF(String filePath) async {
     try {
-      return await FileMagicNumber.detectFileTypeFromPathOrBlob(filePath) ==
-          FileMagicNumberType.pdf;
+      final file = File(filePath);
+      if (!await file.exists()) return false;
+
+      // 파일 핸들 열고
+      final raf = await file.open();
+      // 처음 4바이트만 읽기
+      final header = await raf.read(4);
+      await raf.close();
+
+      if (header.length < 4) return false;
+      return header[0] ==
+              0x25 // '%'
+              &&
+          header[1] ==
+              0x50 // 'P'
+              &&
+          header[2] ==
+              0x44 // 'D'
+              &&
+          header[3] == 0x46; // 'F'
     } catch (e) {
       return false;
     }
@@ -22,8 +48,7 @@ class DocumentUtils {
 
   /// Checks if the given file path has a PDF extension.
   /// Returns `true` if the file has a `.pdf` extension, otherwise `false`.
-  static bool hasPDFExtension(String filePath) =>
-      p.extension(filePath) == ".pdf";
+  static bool hasPDFExtension(String filePath) => p.extension(filePath) == ".pdf";
 
   /// Determines whether the given file path corresponds to an image file.
   ///
@@ -31,10 +56,8 @@ class DocumentUtils {
   /// `.gif`, `.bmp`). If the file has no extension, it is assumed to be an image.
   static Future<bool> isImage(String filePath) async {
     try {
-      final fileType =
-          await FileMagicNumber.detectFileTypeFromPathOrBlob(filePath);
-      return fileType == FileMagicNumberType.png ||
-          fileType == FileMagicNumberType.jpg;
+      final fileType = await FileMagicNumber.detectFileTypeFromPathOrBlob(filePath);
+      return fileType == FileMagicNumberType.png || fileType == FileMagicNumberType.jpg;
     } catch (e) {
       return false;
     }
