eab0d3c2f5af85dfc158448a2ada13be50476a43
Daniel Jimenez <122808735+daniJimen@users.noreply.github.com>
2025-08-06 12:03:08 +0200

    change to test yaml

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PDF Combiner Example',
+      title: 'PDF Combiner Examplee',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
