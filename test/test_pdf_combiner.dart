36486be0d3471d8b7135d94df6a99edfe50ea0c3
Daniel Jimenez <122808735+daniJimen@users.noreply.github.com>
2025-09-04 08:48:46 +0200

    fix test in test_pdf_combiner.dart

--- test/test_pdf_combiner.dart
+++ test/test_pdf_combiner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pdf_combiner/pdf_combiner.dart';
+import 'package:pdf_combiner/pdf_combiner_delegate.dart';
 import 'package:pdf_combiner/responses/pdf_combiner_status.dart';
 import 'package:pdf_combiner/utils/document_utils.dart';
 
@@ -9,19 +10,22 @@ void main() {
   const MethodChannel testChannel = MethodChannel('pdf_combiner');
   group('PdfCombiner', () {
     test("test one image and one pdf", () async {
+      PdfCombiner.isMock = true;
       TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
           .setMockMethodCallHandler(testChannel, (MethodCall methodCall) async {
-        if (methodCall.method == 'createPDFFromMultipleImage') {
-          return '/example/assets/temp/document_1.pdf';
+        if (methodCall.method == 'createPDFFromMultipleImage' ||
+            methodCall.method == 'mergeMultiplePDF') {
+          return './example/assets/temp/document_0.pdf';
         }
         return null;
       });
-      PdfCombiner.isMock = true;
-      DocumentUtils.temporalDir = "/example/assets/temp";
-      var result = await PdfCombiner.createPdfFromImage(
-          "example/assets/image_1.jpeg", 1);
+      DocumentUtils.temporalDir = "./example/assets/temp";
+      var result = await PdfCombiner.generatePDFFromDocuments(
+          inputPaths: ["./example/assets/image_1.jpeg"],
+          outputPath: "./example/assets/temp/document_0.pdf",
+          delegate: PdfCombinerDelegate());
       expect(result.status, PdfCombinerStatus.success);
-      expect(result.outputPath, "/example/assets/temp/document_1.pdf");
+      expect(result.outputPath, "./example/assets/temp/document_0.pdf");
     });
   });
 }
